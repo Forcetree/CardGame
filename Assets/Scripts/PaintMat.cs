b1178c1e044364aa417ecbf81371ec70f415440f
Androidsheep92 <cwoolbert@gmail.com>
2026-07-01 19:50:31 -0400

    Feat: Added Combo Animation
    Added Combo Animation using DG.Tweening

--- Assets/Scripts/PaintMat.cs
+++ Assets/Scripts/PaintMat.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using DG.Tweening;
+
 
 public class PaintMat : FieldMat
 {
@@ -14,7 +16,14 @@ public class PaintMat : FieldMat
         }
         else //stack.Count always greater than 1
         {
-            // Play the combo animation -> not implemented yet
+            DG.Tweening.Sequence s = DOTween.Sequence();
+
+            s.Append(TopperRenderer.DOColor(Color.white, 0.05f).SetEase(Ease.Flash)); // Flash bright
+
+            Color comboColor = CardCombiner.GetVisual<Color>(comboType);
+            s.Join(TopperRenderer.DOColor(comboColor, 0.1f).SetEase(Ease.InSine)); // Change to combo color
+
+            s.Join(Topper.transform.DOPunchScale(new Vector3(0.3f, 0.3f, 0), 0.1f, 1, 0)); // Add a punch scale for a more dynamic effect (vector size adjustment, time, vibrato, elasticity)
         }
         
     }
