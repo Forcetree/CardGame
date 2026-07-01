dbaa1c65f8e00a3dc9660ee9af5dc811c7b6af6d
Androidsheep92 <cwoolbert@gmail.com>
2026-07-01 19:21:55 -0400

    Feat: Play Animation categorizes
    Play Animation now determines category based on the stack.Count
    - Empty Mat Animation
    - Appear Animation
    - Combo Animation

--- Assets/Scripts/PaintMat.cs
+++ Assets/Scripts/PaintMat.cs
@@ -4,7 +4,19 @@ public class PaintMat : FieldMat
 {
     protected override void PlayAnimation()
     {
-        // Play the paint mat animation -> not implemented yet
+        if (stack.Count == 0)
+        {
+            // Play the empty mat animation -> not implemented yet
+        }
+        else if(stack.Count == 1)
+        {
+            // Play the Appear animation -> not implemented yet
+        }
+        else //stack.Count always greater than 1
+        {
+            // Play the combo animation -> not implemented yet
+        }
+        
     }
 }
 
