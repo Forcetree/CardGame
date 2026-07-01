de52194e74f2284dae39566893aed033830239fb
Androidsheep92 <cwoolbert@gmail.com>
2026-07-01 18:50:04 -0400

    Refactor: Remove redundant sorting update
    Changed field mat sprite renderer from single to multi layer.
    
    -Added highlight sprite
    -Added highlight and bottom renderer
    -Expanded value sets for highlight and matIsFull (value sets now self update sprites)
    -Removed pre-staging
    -Removed sorting renderer calls
    -Pulled out Update

--- Assets/Scripts/FieldMat.cs
+++ Assets/Scripts/FieldMat.cs
@@ -7,23 +7,56 @@ public abstract class FieldMat : MonoBehaviour
 {
     // Objects
     public GameObject Topper;
-    public SpriteRenderer TopperRenderer;
-
+    
     // Sprite Refs
     public Sprite basicCard;
-    public Sprite highlightCard;
+    public Sprite highlight;
 
     // Components
-    public SpriteRenderer spriteRenderer;
+    public SpriteRenderer TopperRenderer;
+    public SpriteRenderer BottomRenderer;
+    public SpriteRenderer HighlightRenderer;
+
     public ValueDigitizer valueRenderer;
 
     // Attributes
-    public bool highlighted = false;
+    [SerializeField] private bool _highlighted;
+    public bool highlighted
+    {
+        get { return _highlighted; }
+        set
+        {
+            _highlighted = value;
+            
+            if (_highlighted)
+            {
+                HighlightRenderer.sprite = highlight;
+            }
+            else
+            {
+                HighlightRenderer.sprite = null;
+            }
+        }
+    }
+
+    
+    
+
     public List<Card> stack = new();
 
     public int comboType;
 
-    public bool matIsEmpty = true;
+    [SerializeField] private bool _matIsFull;
+    public bool matIsFull
+    {
+        get { return _matIsFull; } 
+        
+        set  // There is no room for an animation in this integration
+        {
+            _matIsFull = value;
+            Topper.SetActive(_matIsFull); // Show the Topper renderer when the mat is full
+        }
+    }
 
     // Value Digitizer Controller
     [SerializeField] private int _value;
@@ -35,12 +68,6 @@ public abstract class FieldMat : MonoBehaviour
             _value = value;
             valueRenderer.value = _value;
         }
-    }
-
-    public void Awake()
-    {
-        matIsEmpty = true;
-        spriteRenderer.color = new(1f, 1f, 1f, .3f);
     }
 
     public bool TryTarget(Card cCard) // Under reconstruction -> need to check against current stack for possible combos to exapnd the combos to support any number of duplicate cards
@@ -80,15 +107,14 @@ public abstract class FieldMat : MonoBehaviour
         }
 
         highlighted = false;
-        matIsEmpty = false;
+        matIsFull = true;  
     }
 
     protected abstract void PlayAnimation();
 
     private void PlayComboAnimation() // We currently only have one sprite location and swap between field use and topper (why should we not do a bottom and cover it with the topper when needed?
     {
-        TopperRenderer.sortingLayerName = "Topper"; 
-        valueRenderer.UpdateRenderSorting(); // Only required because we must reset the layer on the single sprite renderer that is used for both the topper and value renderer (we should consider separating these into two objects to avoid this issue)
+        // TopperRenderer.color = new Color(1, 1, 1, 0); // Reset the color to white with full opacity before starting the animation
 
         DG.Tweening.Sequence s = DOTween.Sequence();
                 
@@ -111,29 +137,12 @@ public abstract class FieldMat : MonoBehaviour
         value = 0;
         comboType = -1; // Set to default type when clearing the mat -> should be invisible in future
 
-        matIsEmpty = true;
+        matIsFull = false;
 
         // PlayAnimation(); // Can determine that the mat was cleared
 
-        spriteRenderer.color = new(1f, 1f, 1f, .3f);
-
-
-        TopperRenderer.sortingLayerName = "Field"; // Look into a better way to handle the sorting layer for the topper and value renderer (maybe a separate object that handles the topper and value renderer together)
-        valueRenderer.UpdateRenderSorting(); // Only required because we must reset the layer on the single sprite renderer that is used for both the topper and value renderer (we should consider separating these into two objects to avoid this issue)
+        //valueRenderer.UpdateRenderSorting();  // Only required because we must reset the layer on the single sprite renderer that is used for both the topper and value renderer (we should consider separating these into two objects to avoid this issue)
     }
 
-    // Need to find a better approach to live update the sprite state based on a mat state - under construction to refine how the mat displays and takes collisions (check card class for which layers it checks against (unless sorting layer is independant of physics which I think it is))
-    void Update()
-    {
-        Topper.SetActive(!matIsEmpty);
-
-        if (highlighted) // Look for a way to make this change occur when the operations run rather than check each update
-        {
-            spriteRenderer.sprite = highlightCard;
-        }
-        else
-        {
-            spriteRenderer.sprite = basicCard;
-        }
-    }
+    
 }
