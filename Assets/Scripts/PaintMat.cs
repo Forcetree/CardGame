69954c3f3604070dc557782f2177ed8e267d5b64
Androidsheep92 <cwoolbert@gmail.com>
2026-07-01 20:44:48 -0400

    Feat: Added Clear Animation
    Added Clear Animation using DGTweening

--- Assets/Scripts/PaintMat.cs
+++ Assets/Scripts/PaintMat.cs
@@ -8,14 +8,15 @@ public class PaintMat : FieldMat
     {
         Color comboColor = CardCombiner.GetVisual<Color>(comboType);
 
-        if (stack.Count == 0)
+        if (stack.Count == 0) // Clear Animation
         {
-
-            // Play the Clear animation -> not implemented yet
+            DOTween.Sequence()
+                .Append(TopperRenderer.DOColor(new Color(1f, 1f, 1f, 0f), 0.5f).SetEase(Ease.OutExpo));
         }
         else if(stack.Count == 1) // Play Animation for the first card in the stack
         {
             DOTween.Sequence()
+                // Fade in?
                 .Append(Topper.transform.DOPunchScale(new Vector3(0.05f, 0.05f, 1), 0.3f, 1, 0)) // Add a punch scale for a more dynamic effect (vector size adjustment, time, vibrato, elasticity)
                 .Join(TopperRenderer.DOColor(comboColor, 0.1f).SetEase(Ease.InSine)); // Change to combo color
 
