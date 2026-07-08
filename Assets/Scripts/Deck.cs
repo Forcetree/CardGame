b4f61308b6e1ab73d98cf5e22773b4b236243098
Forcetree <forcetree@gmail.com>
2026-07-08 15:53:37 -0700

    Feat: Initial Abstraction of Deck
    Label and identified virtual target methods and added more requirements.
    
    Dependency free changes until Builder objects and other root classes are added.

--- Assets/Scripts/Deck.cs
+++ Assets/Scripts/Deck.cs
@@ -4,42 +4,55 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
 
-public class Deck : MonoBehaviour
+public abstract class Deck : MonoBehaviour
 {
+    [Header("Deck References: Set automatically")]
     [Tooltip("The hand is set during active play by the PlayHandler")]
-    public PlayerHand hand; // Needs Hand object to talk to -> filled on initialization of the deck in PlayHandler
+    public PlayerHand hand; // Never used -> filled on initialization of the deck in PlayHandler -> all methods that require the hand are passed the hand as an argument -> consider removing this variable
     [Tooltip("The PlayHandler is set during active play by the PlayHandler")]
     public PlayHandler PlayHandler; // Needs PlayHandler object to talk to -> filled on initialization of the deck in PlayHandler
-    [Tooltip("The Prefab for Cards is set during active play by the PlayHandler")]
+
+    [Header("Deck Settings: Prefab Links")]
+    [Tooltip("The Prefab for Cards is NOT set during active play by the PlayHandler -> baked in the prefab")]
     public Card CardRef;
 
-    public int DeckSize => cardsInDeck.Count; // Is needed?
-    public List<Card> cardsInDeck = new();
-
-    private Queue<Card> dealBuffer = new();
-
-    [Tooltip("Shows if we are processing a sequence of deals")]
-    public bool isProcessingDealBuffer = false;
-
+    [Header("Deck Settings: Parameters")]
     [Tooltip("Set this for deal rate")]
     public float timeBetweenDeals;
+    [Tooltip("The focus value is where the cards are pushed to before flying to the hand")]
+    public Vector3 drawFocusPos = new(-6, 1.5f, 0);
     [Tooltip("Set this for card speed on focus")]
     public float dealTime;
     [Tooltip("Set this for the type of focus movement")]
     public Ease dealEase;
 
-    [Tooltip("The focus value is where the cards are pushed to before flying to the hand")]
-    public Vector3 drawFocusPos = new(-6, 1.5f, 0);
+    [Header("Deck Vis Indicators")]
+    [Tooltip("Shows if we are processing a sequence of deals")]
+    public bool isProcessingDealBuffer = false;
+    public int DeckSize => cardsInDeck.Count; // Is needed? Not currently used
 
-    public void InitDeck(Card CardRef, PlayHandler handler)
+    // Required -> The deal buffer is used to store cards that are being dealt to the hand. This allows for a smooth deal animation and prevents cards from being dealt too quickly.
+    private Queue<Card> dealBuffer = new();
+
+    // Debating removal
+        // Some use cases need a deck object that does not need a list of cards in the deck (for example, a deck that is used to generate cards on the fly). This allows for a deck object to be created without needing to generate cards in the deck.
+        // Consider moving this down to the child class if required
+    public List<Card> cardsInDeck = new();
+    
+    public virtual void InitDeck()
+    {
+        // Assumes the linking has been baked into the scene/prefab
+    }
+    public void InitDeck(Card CardRef, PlayHandler handler) // Under Construction -> Introducing the Builder objects
     {
         this.CardRef = CardRef;
         this.PlayHandler = handler;
     }
 
-    public bool GenDeck(int deckCount , int typeCount) //added typeCount to allow for generation of a number of card types in the deck
+    public virtual bool GenDeck() { return false; } // Base class does not generate a deck, child classes should override this method to generate a special deck of cards
+    public virtual bool GenDeck(int deckCount , int typeCount) // Overload allows current functionality to be used in child classes that need to generate a deck of cards with a specific number of cards and types
     {
-        for (int i = 0; i < deckCount; i++)
+        for (int i = 0; i < deckCount; i++) // Need a static card method for handling this (encapsulate the card generation logic in the card class and call it from here) -> this will allow for better scalability and maintainability of the code
         {
             Card nCard = Instantiate(CardRef, this.transform.position, Quaternion.identity);
 
@@ -66,6 +79,10 @@ public class Deck : MonoBehaviour
 
         return true;
     }
+
+    public virtual void UpdateDeck() { } // Base class does not update the deck, child classes could override this method to update the deck based on game logic
+    // Consider creating a data object for the deck that can be used to store the deck state and allow for saving/loading of the deck state as well as changing other deck parameters (for example, changing the number of cards in the deck or the types of cards in the deck) -> this would allow for better scalability and maintainability of the code
+    // This could be the same data object used in the init -> consider the core differences between initialization and updating the deck from a functional standpoint
 
     private IEnumerator DealSequence(PlayerHand hand)
     {
